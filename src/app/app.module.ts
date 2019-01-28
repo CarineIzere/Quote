14866726123b44bbf7de79c471d10a06b56ab15d
CarineIzere <carizeree@gmail.com>
2019-01-28 09:33:40 +0200

    add quote details

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { QuoteComponent } from './quote/quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GoalComponent,
+    QuoteComponent,
+    QuoteDetailsComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    GoalFormComponent,
+    QuoteFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
