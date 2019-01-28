ca787c6d460d3fd5e72e5b007b4e8ff9bc50bb3c
CarineIzere <carizeree@gmail.com>
2019-01-28 10:12:49 +0200

    add app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,14 @@
 import { Component } from "@angular/core";
 import { Quote } from "./quote";
+
 @Component({
   selector: "app-root",
-  templateUrl: "./app.component.html"
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  quotes = [
-    new Quote(
-      1,
-      "When you want to make your life feel better and your thoughts more positive"
-    ),
-    new Quote(2, "Buy Cookies"),
-    new Quote(3, "Get new Phone Case"),
-    new Quote(4, "Get Dog Food"),
-    new Quote(5, "Solve math homework"),
-    new Quote(6, "Plot my world domination plan")
-  ];
+  quotes: string[];
+  constructor() {
+    this.quotes = ["Author", "what they say", "add or delete quotes"];
+  }
 }
